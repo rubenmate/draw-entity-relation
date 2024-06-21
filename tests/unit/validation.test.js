5ea1ca2d12ace75973d8e493ca6e67111a46eb5a
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2024-06-21 13:29:26 +0200

    feat: migrate from jest to vitest

--- tests/unit/validation.test.js
+++ tests/unit/validation.test.js
@@ -1,12 +1,20 @@
-import {repeatedEntities } from '../../src/utils/validation'
+import { beforeAll, expect, test } from 'vitest'
+import { repeatedEntities } from "../../src/utils/validation"
 
-// TODO: Add a before all that imports and converts to Object
-// the example json
+let graph;
+
+beforeAll(() => {
+    // Import and convert the example JSON to an Object
+    // Assuming you have an example JSON file located at '../../example.json'
+    graph = require('../../src/utils/examplegraph.json');
+});
+
 test("entities can't have repeated names", () => {
     expect(repeatedEntities(graph)).toBe(false);
 });
+
 test("N:M relations and entities can't have repeated names", () => {
-    // TODO: Access to the N:M relation and set its name to an already
-    // existing entity name
+    // Access the N:M relation and set its name to an already existing entity name
+    // TODO: 
     expect(repeatedEntities(graph)).toBe(true);
 });
