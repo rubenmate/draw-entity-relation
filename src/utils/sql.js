b5e54ea7a02d3d75280a7b84a1c7fa6ce36c8e81
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2024-07-02 17:37:16 +0200

    feat: sanitize accents

--- src/utils/sql.js
+++ src/utils/sql.js
@@ -317,7 +317,65 @@ const getSQLType = (attribute) => {
 };
 
 const sanitizeName = (name) => {
-    return name.replace(/\s+/g, "_");
+    const accentMap = {
+        á: "a",
+        é: "e",
+        í: "i",
+        ó: "o",
+        ú: "u",
+        Á: "A",
+        É: "E",
+        Í: "I",
+        Ó: "O",
+        Ú: "U",
+        ä: "a",
+        ë: "e",
+        ï: "i",
+        ö: "o",
+        ü: "u",
+        Ä: "A",
+        Ë: "E",
+        Ï: "I",
+        Ö: "O",
+        Ü: "U",
+        à: "a",
+        è: "e",
+        ì: "i",
+        ò: "o",
+        ù: "u",
+        À: "A",
+        È: "E",
+        Ì: "I",
+        Ò: "O",
+        Ù: "U",
+        â: "a",
+        ê: "e",
+        î: "i",
+        ô: "o",
+        û: "u",
+        Â: "A",
+        Ê: "E",
+        Î: "I",
+        Ô: "O",
+        Û: "U",
+        ã: "a",
+        õ: "o",
+        ñ: "n",
+        Ã: "A",
+        Õ: "O",
+        Ñ: "N",
+        å: "a",
+        Å: "A",
+        ç: "c",
+        Ç: "C",
+        // Add more mappings if needed
+    };
+
+    return name
+        .split("")
+        .map((char) => accentMap[char] || char)
+        .join("")
+        .replace(/\s+/g, "_");
 };
 
 const createTableSQL = (table) => {
